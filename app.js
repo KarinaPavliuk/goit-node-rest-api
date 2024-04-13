47fa0814a37a57172d64ab58e108d345516d9035
Karina Pavliuk <kpavliuk8@gmail.com>
2024-04-13 14:16:21 +0300

    update

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-
+import multer from "multer";
+import path from "path";
 import contactsRouter from "./routes/contactsRouter.js";
 
 const app = express();
@@ -10,7 +11,29 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
+const tempDir = path.join(process.cwd(), "temp");
+const contactsPath = path.join(process.cwd(), "./db/contacts.json");
+
+const multerConfig = multer.diskStorage({
+  destination: tempDir,
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
+}) 
+
+const upload = multer({
+  storage: multerConfig,
+})
+
+// app.use("/api/contacts", contactsRouter);
+app.get("/api/contacts", (req, res) => {
+  res.json(contactsPath)
+})
+
+app.post("/api/contacts", upload.single("photo"), async (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+})
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
